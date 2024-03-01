e6407edcc57b3a7d107302f420d082a527b36e1f
heppaat <pali.herrer@gmail.com>
2024-03-01 18:06:49 +0100

    backend implementation ongoing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,3 +2,50 @@ import express from "express";
 import { z } from "zod";
 import cors from "cors";
 import fs from "fs/promises";
+
+const server = express();
+
+server.use(cors());
+
+server.use(express.json());
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(`${__dirname}/../database.json`, "utf-8");
+    const users = JSON.parse(data);
+    return users;
+  } catch (error) {
+    return null;
+  }
+};
+
+server.post("/api/register", async (req, res) => {
+  const { email, password, confirmPassword } = req.body;
+
+  const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!isValid) {
+    return res.status(400).send("Invalid email address");
+  }
+
+  if (password.length < 5) {
+    return res.status(400).send("Password must be at least 5 characters long");
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).send("Password and confirmation do not match");
+  }
+
+  const existingUsers = await readFile();
+
+  if (existingUsers.find((user) => user.email === email)) {
+    return res.status(400).send("This email is already registered");
+  }
+
+  const newUser = { email };
+  const updatedUsers = [...existingUsers, newUser];
+
+  await fs.writeFile(
+    `${__dirname}/../database.json`,
+    JSON.stringify(updatedUsers, null, 2)
+  );
+});
